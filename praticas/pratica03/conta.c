381883a97c06e1eba8b56bae3a23082862f95452
Andrew Pereira Viana <andy.viana1@gmail.com>
2026-04-15 15:59:53 +0000

    feat: conclui pratica03. Fecha #ID

--- praticas/pratica03/conta.c
+++ praticas/pratica03/conta.c
@@ -1,2 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "conta.h"
+
+struct conta_t {
+    int numero;
+    float saldo;
+};
+
+Conta* conta_criar(int numero) {
+    Conta* c = (Conta*) malloc(sizeof(Conta));
+    if (c != NULL) {
+        c->numero = numero;
+        c->saldo = 0.0;
+    }
+    return c;
+}
+
+void conta_destruir(Conta* c) {
+    free(c);
+}
+
+void conta_depositar(Conta* c, float valor) {
+    if (valor > 0) {
+        c->saldo += valor;
+    }
+}
+
+int conta_sacar(Conta* c, float valor) {
+    if (valor > 0 && c->saldo >= valor) {
+        c->saldo -= valor;
+        return 1; // sucesso
+    }
+    return 0; // falha
+}
+
+float conta_ver_saldo(Conta* c) {
+    return c->saldo;
+}
--- praticas/pratica03/main.c
+++ praticas/pratica03/main.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 #include "conta.h"
+
+int main() {
+    Conta* c1 = conta_criar(123);
+
+    conta_depositar(c1, 100.0);
+    printf("Saldo: %.2f\n", conta_ver_saldo(c1));
+
+    if (conta_sacar(c1, 30.0)) {
+        printf("Saque realizado!\n");
+    } else {
+        printf("Saque falhou!\n");
+    }
+
+    printf("Saldo final: %.2f\n", conta_ver_saldo(c1));
+
+    conta_destruir(c1);
+    return 0;
+}
