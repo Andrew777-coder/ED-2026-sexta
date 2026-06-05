92a4eed0b2afb2e3fef864b1a2a7b7fd559bdca3
Andrew Viana <andy.viana1@gmail.com>
2026-06-05 11:25:56 +0000

    adiciona pratica 6

--- praticas/pratica05/selecao.h
+++
@@ -1,8 +0,0 @@
-#ifndef SELECAO_H
-#define SELECAO_H
-
-void imprimir_vetor(int arr[], int n);
-int linear_search(int arr[], int n, int k);
-int quick_select(int arr[], int left, int right, int k);
-
-#endif
